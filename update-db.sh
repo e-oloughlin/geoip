0f60572c6f623b464273c7111de97faa8731c9dc
Eoghan O'Loughlin <eoghanoloughlin@gmail.com>
2022-12-18 22:22:57 +0000

    Download script

---
+++ update-db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DB_URL="https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&license_key=$MAXMIND_LICENSE_KEY&suffix=tar.gz"
+SHA256_URL="https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&license_key=$MAXMIND_LICENSE_KEY&suffix=tar.gz.sha256"
+
+echo "Downloading Maxmind's latest DB..."
+curl -o "geoIP.tar.gz" -L "$DB_URL"
+
+echo "Uncompressing file..."
+tar xvf "geoIP.tar.gz"
