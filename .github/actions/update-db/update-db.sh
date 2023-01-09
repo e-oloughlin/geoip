daac1c1a286745e11d68c505885c9669c9d97a82
Eoghan O'Loughlin <eoghanoloughlin@gmail.com>
2023-01-09 20:06:49 +0000

    Start action

---
+++ .github/actions/update-db/update-db.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+SHA_FILENAME="geoIP.tar.gz.sha256"
+ARCHIVE_FILENAME="geoIP.tar.gz"
+
+DB_URL="https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=$MAXMIND_LICENSE_KEY&suffix=tar.gz"
+SHA256_URL="https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=$MAXMIND_LICENSE_KEY&suffix=tar.gz.sha256"
+
+# 1. Download SHA File from maxmind
+
+echo "-> Downloading Maxmind's latest DB SHA..."
+curl -o "$SHA_FILENAME" -L "$SHA256_URL"
+
+IFS=' ' read -r -a ML <<< $(cat $SHA_FILENAME)
+
+MAXMIND_SHA256="${ML[0]}"
+MAXMIND_FILENAME="${ML[1]}"
+
+IFS='.' read -r -a MAXMIND_FOLDERNAME <<< "$MAXMIND_FILENAME"
+
+# 2. Download SHA File from S3
+
+echo "-> Downloading S3's latest DB SHA..."
+aws s3 cp s3://siorc/ip/$SHA_FILENAME "./S3_$SHA_FILENAME"
+
+IFS=' ' read -r -a S3L <<< $(cat "S3_$SHA_FILENAME")
+S3_SHA256="${S3L[0]}"
+
+# 3. Compare SHA Files and if different, download DB and upload to S3
+
+echo "-> Comparing maxmind to s3..."
+if [ "$MAXMIND_SHA256" != "$S3_SHA256" ]; then
+	echo "-> New update available. Downloading Maxmind's latest DB..."
+	curl -o "$ARCHIVE_FILENAME" -L "$DB_URL"
+	tar -xvf "$ARCHIVE_FILENAME"
+
+	# Upload SHA to S3
+	echo "-> Uploading new SHA & DB file to S3..."
+	aws s3 cp "$MAXMIND_FOLDERNAME/GeoLite2-City.mmdb" s3://siorc/ip/GeoLite2-City.mmdb
+	aws s3 cp "$SHA_FILENAME" s3://siorc/ip/$SHA_FILENAME
+
+	echo "{update-available}={true}" >> $GITHUB_OUTPUT
+else
+	echo "-> No update available."
+	echo "{update-available}={false}" >> $GITHUB_OUTPUT
+fi
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # GEOIP lookup service
+
+## Notes
+
+Rather than upload the DB to S3 and read from that at application startup:
+
+1. Create a [lambda container image](https://docs.aws.amazon.com/lambda/latest/dg/images-create.html)
+2. Upon new DB release, build the image which has the DB baked in and push that image to ECR.
+3. Deploy the image to lambda to be run serverless.
+4. Remove use of express [if appropriate](https://medium.com/dailyjs/six-reasons-why-you-shouldnt-run-express-js-inside-aws-lambda-102e3a50f355).
